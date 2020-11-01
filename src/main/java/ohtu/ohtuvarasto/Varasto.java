589a39d373cb7149663f6a2c67eda7c66ca69fdc
Antti Ollikkala <antti@huom.io>
2020-11-01 23:14:50 +0200

    rikki

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -78,6 +78,6 @@ public class Varasto {
 
     // --- Merkkijonoesitys Varasto-oliolle: ----
     public String toString() {
-        return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
+        return ("salfdo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
     }
 }
